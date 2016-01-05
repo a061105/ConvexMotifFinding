137d46bbb464354531d7377c36b8da01e93ff926
Jiongdys <zhangjiong724@utexas.edu>
2016-01-05 22:16:26 +0800

    apple 30

--- main.cpp
+++ main.cpp
@@ -20,9 +20,9 @@ extern MAT_D OptPhaseTwo(	MAT_D CC,
 
 int main()
 {
-	string	    Seq="0110000101100001011000010110000101100001";
-	string    InSeq="0110000101100001011000010110000101100001";
-	string    word="aaaaa";
+	string	    Seq="0110000101110000011100000110110001100101";
+	string    InSeq="0110000101110000011100000110110001100101";
+	string    word="apple";
 	// construct C
 	vector<double> col0(Tseq,cost_un),col1(Tseq,0.0);
 	MAT_D C(J,col1);
@@ -36,33 +36,33 @@ int main()
 	// continue construct C adding a little bias towards rank lower patterns
 	for(int t=0; t<Tseq; t++){
 		int dig=0;
-		double eps=0.01;
+		double eps=0.001;
 		if(InSeq[t]=='1') dig=1;
 		for(int kk=0; kk<KG; kk++){
 			for(int j=0; j<2*L; j++){
 				if(j%2!=dig){	// j odd assign to 0, j even assign to 1
-					C[(1+2*L*kk)+j][t]=cost_mis;//+eps*kk;
+					C[(1+2*L*kk)+j][t]=cost_mis+eps*kk;
 				}else{
-					//C[(1+2*L*kk)+j][t]=eps*kk;
+					C[(1+2*L*kk)+j][t]=eps*kk;
 				}
 			}
 		}
 	}
 	
 
-	//----------------------initialize W1, all optimal--------------------------
-	/*MAT_D W1(J,col1);
+	//----------------------initialize W2, all optimal--------------------------
+	/*MAT_D W2(J,col1);
 	for(int i=1; i<=word_length; i++){ // consider ith char
 		int index1=word[i-1]-'a';      // ascii of ith char-97
 		for(int j=1; j<=L; j++){	// jth digit of ith char's pattern
 			int t=(i-1)*L+j-1;		//pisition in seq
 			int dig=0;
 			if(Seq[t]=='1') dig=1;	//correct assign should be 1 or 0
-			W1[(2*j-2)+dig+(2*L*index1+1)][t]=1;
+			W2[(2*j-2)+dig+(2*L*index1+1)][t]=1;
 		}
 	}
 	cout<<"Optimal is:"<<endl;
-	ResOut(W1);*/
+	ResOut(W2);*/
 	//----------------------------------------------------------------------------
 	MAT_D Y(J,col1);  // initialize Y with all zeros
 	// start rowlling
