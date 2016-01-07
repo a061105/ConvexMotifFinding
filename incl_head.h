158dd6d0daef402821d975aa729a2dbc9f15a039
Jiongdys <zhangjiong724@utexas.edu>
2016-01-07 17:35:43 +0800

    apppllll

--- incl_head.h
+++ incl_head.h
@@ -6,12 +6,14 @@
 #include<string>
 #include<vector>
 #include<stdio.h>
+#include<stdlib.h>
+#define random ((rand()%100)/100.0)
 #define MAX_NUMBER 999999
 using namespace std;
 typedef vector<vector<double> > MAT_D; 
 
-	const int word_length=6,L=8;
+	const int word_length=8,L=8;
 	const int KG=3,Kopt=3;
 	const int J=2*KG*L+1,Tseq=word_length*L;
-	const int update_num=1000, Inner_num=30;
+	const int update_num=100, Inner_num=30;
 	const double mu=0.1,cost_un=1.0,cost_mis=1.0;
--- main.cpp
+++ main.cpp
@@ -25,9 +25,9 @@ extern double DisToOne(		MAT_D WW1);
 
 int main()
 {
-	string	    Seq="011000010111000001110000011011000110110001101100";
-	string    InSeq="011000010111000001110000011011000110110001101100";
-	string    word="applll";
+	string	    Seq="0110000101110000011100000111000001101100011011000110110001101100";
+	string    InSeq="0110000101110000011100000111000001101100011011000110110001101100";
+	string    word="apppllll";
 	// construct C
 	vector<double> col0(Tseq,cost_un),col1(Tseq,0.0);
 	MAT_D C(J,col1);
@@ -41,14 +41,14 @@ int main()
 	// continue construct C adding a little bias towards rank lower patterns
 	for(int t=0; t<Tseq; t++){
 		int dig=0;
-		double eps=0.003;
+		double eps=0.01;
 		if(InSeq[t]=='1') dig=1;
 		for(int kk=0; kk<KG; kk++){
 			for(int j=0; j<2*L; j++){
 				if(j%2!=dig){	// j odd assign to 0, j even assign to 1
-					C[(1+2*L*kk)+j][t]=cost_mis+eps*kk;
+					C[(1+2*L*kk)+j][t]=cost_mis+eps*(kk+random);
 				}else{
-					C[(1+2*L*kk)+j][t]=eps*kk;
+					C[(1+2*L*kk)+j][t]=eps*(kk+random);
 				}
 			}
 		}
@@ -73,23 +73,25 @@ int main()
 	// start rowlling
 	for(int Iter=0; Iter<update_num; Iter++){
 		// Optimization Phase One
-		cout<<"Phase One"<<endl;
+		//cout<<"Phase One"<<endl;
 		for(int Inner_iter=0; Inner_iter<Inner_num; Inner_iter++){
 			int k_num=Iter+1;
 		W1=OptPhaseOne(C,W1,W2,Y,k_num);
-		cout<<"Loss:"<<LossfuncW1(C,W1)<<";  "<<"Diff:"<<diff(W1,W2)<<endl;
+		//cout<<"Loss:"<<LossfuncW1(C,W1)<<";  "<<"Diff:"<<diff(W1,W2)<<endl;
 		}
 		//Optimization Phase Two
-		cout<<"Phase Two"<<endl;
+		//cout<<"Phase Two"<<endl;
 		for(int Inner_iter=0; Inner_iter<Inner_num; Inner_iter++){
 			int k_num=Iter+1;
 		W2=OptPhaseTwo(C,W1,W2,Y,k_num);
-		cout<<"Loss:"<<LossfuncW1(C,W1)<<";  "<<"Diff:"<<diff(W1,W2)<<endl;
+		//cout<<"Loss:"<<LossfuncW1(C,W1)<<";  "<<"Diff:"<<diff(W1,W2)<<endl;
 		}
 		//Optimization Phase Three
 		Y=UpdateY(W1,W2,Y);
-		cout<<"-----End Outer Step"<<Iter+1<<"-----"<<endl;
-		cout<<"---Loss func value:"<<LossfuncW1(C,W1)<<"---"<<endl;
+		//cout<<"-----End Outer Step"<<Iter+1<<"-----"<<endl;
+		double diffW12=diff(W1,W2);
+		cout<<"Loss:"<<LossfuncW1(C,W1)<<";  "<<"Diff:"<<diffW12<<endl;
+		if(diffW12<1e-5) break;
 		//ResOut(W1);
 	}
 	cout<<"End output W1:"<<endl;
