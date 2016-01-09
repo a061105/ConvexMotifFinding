1cffbfa0b6c4f6c03f284badc92f896c79df4f12
Jiongdys <zhangjiong724@utexas.edu>
2016-01-09 23:39:36 +0800

    k=6

--- incl_head.h
+++ incl_head.h
@@ -19,7 +19,7 @@ typedef vector<vector<double> > MAT_D;
 	const string word="auusssttttiiiiinnnnnn";	
 	const int word_length=word.size();
 	const int L=8;
-	const int KG=5,Kopt=5;
+	const int KG=6,Kopt=6;
 	const int J=2*KG*L+1,Tseq=word_length*L;
 	const int update_num=1000, Inner_num=30;
 	const double mu=0.1,cost_un=1.0,cost_mis=1.0;
