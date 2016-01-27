3c124af0b2945486df9d85c9135aef7db4e6f16e
Jiongdys <zhangjiong724@utexas.edu>
2016-01-26 21:12:06 -0600

    1000

--- incl_head.h
+++ incl_head.h
@@ -22,7 +22,7 @@ typedef vector<vector<double> > MAT_D;
 	const int L=6,Lmin=5;
 	const int KG1=64,KG2=32,Kopt=7;  // from 00000(means space) 00001(a) ... 11010(z)
 	const int J1=KG1*2*L+1,J2=KG2*2*Lmin,Tseq=word_length*Lmin+space_num;
-	const int update_num=1, Inner_num=30;
+	const int update_num=1000, Inner_num=30;
 	const double mu=0.3,cost_un=1.0,cost_mis=1.5;
 	const double prefer=0.1,short_prefer=prefer/(L+Lmin);
 	const double inner_eps=1e-5, outer_eps=1e-5;
